7d2a2cffa722903b56f95205f2823626a80f241d
Ludwig <ludwig@frostudios.com>
2024-01-22 10:13:08 +0100

    fixed?

---
+++ sec_models/0.5/convertHex.py
@@ -0,0 +1,12 @@
+from readSignatures import readSignatures
+
+def convertHex(signatures):
+    for key, value in signatures.items():
+         bytes_data = bytes.fromhex(value)
+         print(key, ":",value, ":", bytes_data)
+
+         
+        
+
+
+convertHex(readSignatures())
