1175e4b80af8f976a4f6b6f759c2db137040c6a5
Ludwig <ludwig@frostudios.com>
2024-02-28 09:24:30 +0100

    fix

--- sec_models/0.5.1/main.py
+++ sec_models/0.5.1/main.py
@@ -1,52 +1,12 @@
 from file_traversal import file_traversal
-from convert import toHex
-from convert import toByte
 from read_signatures import read_signatures
 
-# Itereate through file directory
-
-
-files = file_traversal("/Users/0x/Documents/GitHub/SecurityModels/sec_models/ourDir")
-count = 0
-
-
+files = file_traversal()
+signatures = read_signatures()
 
 for file in files:
-
-    # Take each file and put them into toHex()
-    # Take each file and put them into toBytes()
-    currentFileHex = toHex(file)
-    currentFileByte = toByte(file)
-  
-   
-    signatures = read_signatures()  # Dict {virus:hex}
-    
-    for key, value in signatures.items():
-        virus_hex = value
-        #print(virus_hex)
-        #print(value)
-
-        # take first virus hex value and compare to file in hex
-
-        
-        # if currentFileHex.find(str(virus_hex)) is not True:
-        if str(virus_hex) in currentFileHex:
-            
-            count += 1
-
-            print("match! this virus found: ", key)
-
-    print("File in hex: " + currentFileHex + "\n")
-    print("Virus in hex " + str(virus_hex))
-    # print("HEX:\n", currentFileHex)
-    # print("BYTE:\n",currentFileByte)
-
-    # Itereate through signatures
-
-    # take each sing and put into toHex()
-
-    # take each sing and put into toBytes()
-
-    # Compare toHex(File[i]) == toHex(sign[i])
-    # Compare toBytes(File[i]) == toBytes(sign[i])
-print(count)
+    with open(file, 'rb') as f:
+        data = f.read()
+        for key, values in signatures.items():
+            if values in data:
+                print("MATCH: ", key, "in ", file)
